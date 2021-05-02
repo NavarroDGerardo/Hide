bdcbce32f0b86d832170bbcb2f8c97b9535cb9a4
NavarroDGerardo <dgnvrrg@gmail.com>
2021-05-02 18:18:06 -0500

    crow AI progress

---
+++ Hide/Assets/_Scripts/Crowd.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using System;
+
+public class Crowd : MonoBehaviour
+{
+    List<List<Vector3>> positions;
+
+    public GameObject prefab;
+    public int numberPeople;
+    private GameObject[] people;
+    public float timerToUpdate = 5.0f;
+    public float timer;
+    public float dt;
+
+    struct Data
+    {
+        public Vector3[] positions;
+
+        public Data(Vector3[] _positions)
+        {
+            positions = _positions;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        people = new GameObject[numberPeople];
+        for(int i = 0; i < people.Length; i++)
+        {
+            people[i] = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+
+        positions = new List<List<Vector3>>();
+
+        Vector3 pos = transform.localPosition;
+        string json = EditorJsonUtility.ToJson(pos);
+        //string call = "WAAAASAAAAP";
+        StartCoroutine(SendData(json));
+        timer = timerToUpdate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        dt = 1.0f - (timer / timerToUpdate);
+
+        if (timer < 0)
+        {
+            timer = timerToUpdate;
+            Vector3 fakepos = new Vector3(3.44f, 0, -15.70f);
+            string json = EditorJsonUtility.ToJson(fakepos);
+            StartCoroutine(SendData(json));
+        }
+
+        if (positions.Count > 1)
+        {
+            for (int s = 0; s < people.Length; s++)
+            {
+                List<Vector3> last = positions[positions.Count - 1];
+                List<Vector3> prevLast = positions[positions.Count - 2];
+                Vector3 interpolated = Vector3.Lerp(prevLast[s], last[s], dt);
+                people[s].transform.localPosition = interpolated;
+
+                Vector3 dir = last[s] - prevLast[s];
+                people[s].transform.rotation = Quaternion.LookRotation(dir);
+            }
+        }
+    }
+
+    IEnumerator SendData(string data)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("bundle", "the data");
+        string url = "http://localhost:8585";
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(data);
+            www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-type", "application/json");
+
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                List<Vector3> newPositions = new List<Vector3>();
+                string txt = www.downloadHandler.text.Replace('\'', '\"');
+                txt = txt.TrimStart('"', '{', 'd', 'a', 't', 'a', ':', '[');
+                txt = "{\"" + txt;
+                txt = txt.TrimEnd(']', '}');
+                txt = txt + '}';
+                Debug.Log(txt);
+                string[] strs = txt.Split(new string[] { "}, {" }, StringSplitOptions.None);
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    strs[i] = strs[i].Trim();
+                    if (i == 0) strs[i] = strs[i] + '}';
+                    else if (i == strs.Length - 1) strs[i] = '{' + strs[i];
+                    else strs[i] = '{' + strs[i] + '}';
+                    Vector3 test = JsonUtility.FromJson<Vector3>(strs[i]);
+                    newPositions.Add(test);
+                }
+
+                List<Vector3> poss = new List<Vector3>();
+                for (int i = 0; i < people.Length; i++)
+                {
+                    poss.Add(newPositions[i]);
+                }
+
+                positions.Add(poss);
+            }
+        }
+    }
+}
---
+++ Hide/Assets/_Scripts/Python/server.py
@@ -0,0 +1,82 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import json
+
+import numpy as np
+from boid import Boid
+
+width = 30
+height = 30
+
+flock = [Boid(*np.random.rand(2)*10, width, height) for _ in range(20)]
+
+def updatePosition():
+    global flock
+    positions = []
+    for boid in flock:
+        boid.apply_behaviour(flock)
+        boid.update()
+        pos = boid.edges()
+        positions.append(pos)
+    return positions
+
+def positionsToJSON(ps):
+    posDICT = []
+    for p in ps:
+        pos = {
+            "x" : p[0],
+            "z" : p[1],
+            "y" : p[2]
+        }
+        posDICT.append(pos)
+    return json.dumps(posDICT)
+
+
+class Server(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        #post_data = self.rfile.read(content_length)
+        post_data = json.loads(self.rfile.read(content_length))
+        #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), post_data.decode('utf-8'))
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), json.dumps(post_data))
+
+        # x = post_data['x'] * 10
+        # y = post_data['y']
+        # z = post_data['z'] * 10
+
+        # position = {"x" : x, "y" : y, "z" : z}
+
+        positions = updatePosition()
+        self._set_response()
+        resp = "{\"data\":" + positionsToJSON(positions) + "}"
+        self.wfile.write(resp.encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=Server, port=8585):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd =server_class(server_address, handler_class)
+    logging.info("Starting httpd...\n")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.ingo("Stopping http...\n")
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(por=int(argv[1]))
+    else:
+            run()
