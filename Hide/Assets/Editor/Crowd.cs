34b84e39cb6c677c124f4c4582273ea14660b638
NavarroDGerardo <dgnvrrg@gmail.com>
2021-06-03 02:27:28 -0500

    carpeta Editor

--- Hide/Assets/Editor/Crowd.cs
+++ Hide/Assets/Editor/Crowd.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.Networking;
 using System;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
 
 public class Crowd : MonoBehaviour
 {
